ff12ee309ec00dee0cecf0e5ed9462410c9bf20b
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-28 13:31:57 +0300

    Adds factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -13,8 +13,9 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\Category::class, function (Faker $faker) {
+$factory->define(App\Product::class, function (Faker $faker) {
     return [
-        'title' => $faker->sentence
+        'title' => $faker->sentence,
+        'category_id' => $faker->numberBetween(1,100)
     ];
 });
