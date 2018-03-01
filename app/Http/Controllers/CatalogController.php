e63830d9acf4c7641a4490cacd0d520d34cba0d8
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-01 10:53:11 +0300

    Creates resource methods

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,34 +2,103 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
 use Illuminate\Http\Request;
+use App\Category,
+    App\Product;
 
 class CatalogController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-//        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
+     * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
+        return "Some list of categories";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string $alias
+     * @return \Illuminate\Http\Response
+     */
+    public function show($alias)
+    {
         $product_id = 1;
 
-        /** @var \App\Product $product */
-        $product = Product::find($product_id);
+        /** @var \App\Category $category */
+        $category = Category::where(['alias' => $alias])->first();
+
+
+        if (!$category){
+            die('No category');
+        }
+
+        $products = Product::where(['category_id' => $category->id])->get();
+//        var_dump($category->id);
+//        var_dump($products);
 //        var_dump($product->categories()->first());
 
-        return view('catalog.product.preview')->with('product', $product);
+        return view('catalog.category.alias')->with([
+            'category' => $category,
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
