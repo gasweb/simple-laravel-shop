16ec7ebee0125b3753485a866950556f7c957d8b
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-02 23:46:16 +0300

    Adds edit template

---
+++ resources/views/catalog/category/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/app')
+@section('content')
+    <h1>{{ trans('category.create_header') }}</h1>
+
+    {!! Form::open(['action' => ['CatalogController@update', $category->id], 'method' => 'POST']) !!}
+    {{ Form::inputText('title', trans('category.create_label'), $category->title, ['placeholder' => trans('category.create_placeholder')]) }}
+    {{ Form::inputText('alias', trans('category.create_alias_label'), $category->alias, ['placeholder' => trans('category.create_alias_placeholder')]) }}
+
+    {{-- If categories exists --}}
+    @if($categories_list)
+        {{ Form::formSelect('parent', trans('category.create_label_parent_select'), $categories_list, $category->parent_id, []) }}
+    @endif
+    {{ Form::inputSubmit('submit', ['class' => 'btn btn-primary']) }}
+    {!! Form::close() !!}
+
+@endsection
