d11a9bfb4e7847c55c5450d42ac7a26842bf8bd8
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-02 16:36:46 +0300

    Creates create category template

---
+++ resources/views/catalog/category/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/app')
+@section('content')
+    <h1>{{ trans('category.create_header') }}</h1>
+
+    {!! Form::open(['action' => 'CatalogController@store', 'method' => 'POST']) !!}
+        {{ Form::inputText('title', trans('category.create_label'), '', ['placeholder' => trans('category.create_placeholder')]) }}
+        {{ Form::inputText('alias', trans('category.create_alias_label'), '', ['placeholder' => trans('category.create_alias_placeholder')]) }}
+
+    {{-- If categories exists --}}
+    @if($categories_list)
+        {{ Form::formSelect('parent', trans('category.create_label_parent_select'), $categories_list, '', []) }}
+    @endif
+        {{ Form::inputSubmit('submit', ['class' => 'btn btn-primary']) }}
+    {!! Form::close() !!}
+
+@endsection
