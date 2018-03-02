ab1a67a5027181bd0b865b22d8859999ac95f5d3
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-02 16:36:29 +0300

    Creates category translates file

---
+++ resources/lang/en/category.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'create_label' => 'Create new category',
+    'create_header' => 'New category',
+    'create_placeholder' => 'Enter category title',
+    'create_label_parent_select' => 'Parent category',
+    'create_alias_label' => 'Alias',
+    'create_alias_placeholder' => 'Enter alias',
+
+];
