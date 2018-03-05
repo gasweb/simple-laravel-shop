765dcca540e8d47364126d638939d80bd3a83a67
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-05 13:43:23 +0300

    Adds autocomplete attribute and _method hidden input

--- resources/views/catalog/category/edit.blade.php
+++ resources/views/catalog/category/edit.blade.php
@@ -3,14 +3,21 @@
     <h1>{{ trans('category.create_header') }}</h1>
 
     {!! Form::open(['action' => ['CatalogController@update', $category->id], 'method' => 'POST']) !!}
-    {{ Form::inputText('title', trans('category.create_label'), $category->title, ['placeholder' => trans('category.create_placeholder')]) }}
-    {{ Form::inputText('alias', trans('category.create_alias_label'), $category->alias, ['placeholder' => trans('category.create_alias_placeholder')]) }}
+    {{ Form::inputText('title', trans('category.create_label'), $category->title, [
+    'placeholder' => trans('category.create_placeholder'),
+    'autocomplete' => 'off'
+    ]) }}
+    {{ Form::inputText('alias', trans('category.create_alias_label'), $category->alias, [
+    'placeholder' => trans('category.create_alias_placeholder'),
+    'autocomplete' => 'off'
+    ]) }}
 
     {{-- If categories exists --}}
     @if($categories_list)
         {{ Form::formSelect('parent', trans('category.create_label_parent_select'), $categories_list, $category->parent_id, []) }}
     @endif
     {{ Form::inputSubmit('submit', ['class' => 'btn btn-primary']) }}
+    {{ Form::hidden('_method', 'PUT') }}
     {!! Form::close() !!}
 
 @endsection
