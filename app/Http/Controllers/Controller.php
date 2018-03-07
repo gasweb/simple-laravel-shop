186f3ed046975a10b1b149b6c90bcd0eafab8dfe
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-07 15:51:16 +0300

    Adds phpdoc

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs,
+    Illuminate\Routing\Controller as BaseController,
+    Illuminate\Foundation\Validation\ValidatesRequests,
+    Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Class Controller
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
