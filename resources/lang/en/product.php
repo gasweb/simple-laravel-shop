099ab61f97138b0425fb74eda0db523fe749019b
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-08 23:20:41 +0300

    Updates translates

---
+++ resources/lang/en/product.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'create_label' => 'Create new product',
+    'create_header' => 'New product',
+    'create_placeholder' => 'Enter product title',
+    'create_label_parent_select' => 'Parent product',
+    'create_alias_label' => 'Alias',
+    'create_alias_placeholder' => 'Enter alias',
+    'admin_list_header' => 'Products',
+    'admin_list_table_title' => 'Title',
+    'admin_list_table_alias' => 'Alias',
+    'admin_list_table_parent' => 'Parent',
+    'admin_new_product_link_text' => 'Create new product',
+    'admin_menu_title' => 'Products',
+    'admin_category_select_empty_option' => 'Select category',
+    'admin_category_select_label' => 'Category',
+
+
+
+];
---
+++ resources/lang/ru/category.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'create_label' => 'Создать новую категорию',
+    'create_header' => 'Новая категория',
+    'create_placeholder' => 'Введите заголовок категории',
+    'create_label_parent_select' => 'Родительская категория',
+    'create_alias_label' => 'Параметр URL',
+    'create_alias_placeholder' => 'Введите параметр URL',
+    'admin_list_header' => 'Категории',
+    'admin_list_table_title' => 'Название',
+    'admin_list_table_alias' => 'Параметр URL',
+    'admin_list_table_parent' => 'Родмтель',
+    'admin_new_category_link_text' => 'Создать новую категорию',
+    'admin_menu_title' => 'Категории',
+    'admin_category_select_empty_option' => 'Выбрать категорию',
+
+
+
+];
