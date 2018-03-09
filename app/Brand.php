cdfe8572a1dffcec963591b780e5f25c447455cc
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-09 23:24:53 +0300

    Adds phpdoc properties

--- app/Brand.php
+++ app/Brand.php
@@ -4,6 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Brand
+ * @package App
+ * @property string title
+ * @property string alias
+ * @property integer id
+ */
 class Brand extends Model
 {
     public function product()
