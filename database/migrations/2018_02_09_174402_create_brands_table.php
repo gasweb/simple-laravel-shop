a690b5fd78517681d91dae3ca24b82b868bc0922
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-09 22:37:40 +0300

    Adds alias key

--- database/migrations/2018_02_09_174402_create_brands_table.php
+++ database/migrations/2018_02_09_174402_create_brands_table.php
@@ -15,8 +15,8 @@ class CreateBrandsTable extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('alias');
             $table->string('title');
-            $table->string('title_translate')->nullable();
             $table->timestamps();
         });
     }
