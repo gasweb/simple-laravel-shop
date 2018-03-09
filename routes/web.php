6d6965d41f29a0cb3c8ab2b29ea95ede89769a40
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-09 23:21:19 +0300

    Creates Brand route

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,7 @@ Route::get('/', function () {
 
 Route::resource('admin/catalog','Catalog\Admin\CatalogController');
 Route::resource('admin/product','Product\Admin\ProductController');
+Route::resource('admin/brand','Brand\Admin\BrandController');
 Route::post('admin/catalog/{id}/image-store', 'Catalog\Admin\CatalogController@imageStore')->name('category.image.store');
 Route::post('admin/product/{id}/image-store', 'Product\Admin\ProductController@imageStore')->name('product.image.store');
+Route::post('admin/brand/{id}/image-store', 'Brand\Admin\BrandController@imageStore')->name('brand.image.store');
