e26e51882b206d2d5d5cb71c42e80cb7182285fe
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-09 23:21:45 +0300

    Creates language file

---
+++ resources/lang/en/brand.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'create_label' => 'Create new brand',
+    'create_header' => 'New brand',
+    'create_placeholder' => 'Enter brand title',
+    'create_alias_label' => 'Alias',
+    'create_alias_placeholder' => 'Enter alias',
+    'admin_list_header' => 'Brands',
+    'admin_list_table_title' => 'Title',
+    'admin_list_table_alias' => 'Alias',
+    'admin_list_table_parent' => 'Parent',
+    'admin_new_brand_link_text' => 'Create new brand',
+    'admin_menu_title' => 'Brands',
+
+
+
+];
