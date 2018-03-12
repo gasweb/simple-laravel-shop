c3af86064127893a8665a06f80565d914b585ea8
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-12 16:32:23 +0300

    Adds phpdoc block, fix label title, fix category select, adds parent select

--- resources/views/catalog/product/admin/edit.blade.php
+++ resources/views/catalog/product/admin/edit.blade.php
@@ -1,3 +1,4 @@
+<?php /** @var \App\Product $product  */ ?>
 @extends('layout.admin.app')
 @section('content')
     <h1>{{ trans('product.edit_header') }}</h1>
@@ -10,7 +11,7 @@
     </div>
 
     {!! Form::open(['action' => ['Product\Admin\ProductController@update', $product->id], 'method' => 'POST']) !!}
-    {{ Form::inputText('title', trans('product.create_label'), $product->title, [
+    {{ Form::inputText('title', trans('product.label_title'), $product->title, [
     'placeholder' => trans('product.create_placeholder'),
     'autocomplete' => 'off'
     ]) }}
@@ -21,7 +22,12 @@
 
     {{-- If categories exists --}}
     @if($categories_list)
-        {{ Form::formSelect('parent', trans('product.create_label_parent_select'), $categories_list, $product->parent_id, []) }}
+        {{ Form::formSelect('category', trans('product.create_label_parent_select'), $categories_list, $product->category_id, []) }}
+    @endif
+
+    {{-- If categories exists --}}
+    @if($products_list)
+        {{ Form::formSelect('parent', trans('product.create_label_parent_select'), $products_list, $product->parent_id, []) }}
     @endif
 
     {{-- If brands exists --}}
