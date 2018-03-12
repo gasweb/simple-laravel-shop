3a59d1eb06ddc27fca7b1b6a8e1c3ed94c7fe220
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-12 15:45:33 +0300

    Change routes to backoffice

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('admin/catalog','Catalog\Admin\CatalogController');
-Route::resource('admin/product','Product\Admin\ProductController');
-Route::resource('admin/brand','Brand\Admin\BrandController');
-Route::post('admin/catalog/{id}/image-store', 'Catalog\Admin\CatalogController@imageStore')->name('category.image.store');
-Route::post('admin/product/{id}/image-store', 'Product\Admin\ProductController@imageStore')->name('product.image.store');
-Route::post('admin/brand/{id}/image-store', 'Brand\Admin\BrandController@imageStore')->name('brand.image.store');
+Route::get('/backoffice', 'Backoffice\Admin\AdminController@index')->name('backoffice.index');
+Route::resource('backoffice/catalog','Catalog\Admin\CatalogController');
+Route::resource('backoffice/product','Product\Admin\ProductController');
+Route::resource('backoffice/brand','Brand\Admin\BrandController');
+Route::post('backoffice/catalog/{id}/image-store', 'Catalog\Admin\CatalogController@imageStore')->name('category.image.store');
+Route::post('backoffice/product/{id}/image-store', 'Product\Admin\ProductController@imageStore')->name('product.image.store');
+Route::post('backoffice/brand/{id}/image-store', 'Brand\Admin\BrandController@imageStore')->name('brand.image.store');
