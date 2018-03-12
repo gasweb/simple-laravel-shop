93fa9feb43005ff67d80142dff6143c34ebf6173
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-12 22:50:38 +0300

    Adds phpdoc and fix image url

--- resources/views/catalog/brand/admin/edit.blade.php
+++ resources/views/catalog/brand/admin/edit.blade.php
@@ -1,6 +1,14 @@
+<?php /** @var \App\Brand $brand */ ?>
 @extends('layout.admin.app')
 @section('content')
     <h1>{{ trans('brand.create_header') }}</h1>
+
+    <div class="jumbotron">
+        @if($brand->image)
+            <img src="{{ url('/'.$brand->image->src_small) }}" alt="">
+        @endif
+        {{ Form::fileUpload('brand_image', ['Brand\Admin\BrandController@imageStore', $brand->id], trans('main.upload_image'), [], trans('main.upload'), ['class' => 'btn btn-primary']) }}
+    </div>
 
     {!! Form::open(['action' => ['Brand\Admin\BrandController@update', $brand->id], 'method' => 'POST']) !!}
     {{ Form::inputText('title', trans('brand.create_label'), $brand->title, [
@@ -15,11 +23,4 @@
     {{ Form::inputSubmit('submit', ['class' => 'btn btn-primary']) }}
     {{ Form::hidden('_method', 'PUT') }}
     {!! Form::close() !!}
-
-    <div class="jumbotron">
-        @if($brand->image)
-            <img src="{{ $brand->image->src_small }}" alt="">
-        @endif
-        {{ Form::fileUpload('brand_image', ['Brand\Admin\BrandController@imageStore', $brand->id], trans('main.upload_image'), [], trans('main.upload'), ['class' => 'btn btn-primary']) }}
-    </div>
 @endsection
