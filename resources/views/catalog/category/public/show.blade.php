d07534bcb912437d774a59c7d98459e9e07bc01c
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-13 15:30:05 +0300

    Adds breadcrumbs and product link

--- resources/views/catalog/category/public/show.blade.php
+++ resources/views/catalog/category/public/show.blade.php
@@ -1,17 +1,21 @@
 @extends('layout.app')
 @section('content')
+    {{ Breadcrumbs::render('catalog', $category) }}
     @if(count($products) > 0)
         <div class="row">
             <?php /** @var \App\Product $product */ ?>
             @foreach($products as $product)
                 <div class="col-sm-3">
-                    <img src="{{ $product->getImagePath() }}" alt="" class="img-responsive card-img">
-                    {{ $product->title }}
+                    <a href="{!! route('product.slug', ['slug' => $product->alias ]); !!}">
+                        <img src="{{ $product->getImagePath() }}" alt="" class="img-responsive card-img">
+                    </a>
+                    <a href="{!! route('product.slug', ['slug' => $product->alias ]); !!}">
+                        {{ $product->title }}
+                    </a>
                 </div>
             @endforeach
         </div>
     @endif
-
     <div class="pagination-wrapper">
         {{ $products->links() }}
     </div>
