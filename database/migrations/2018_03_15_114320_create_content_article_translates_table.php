a3ec2d16abfdb73aa140329469493aa77f207d72
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-15 17:53:31 +0300

    Adds content article and category

--- database/migrations/2018_03_15_114320_create_content_article_translates_table.php
+++ database/migrations/2018_03_15_114320_create_content_article_translates_table.php
@@ -16,13 +16,15 @@ class CreateArticleTranslatesTable extends Migration
         Schema::create('article_translates', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('article_id');
-            $table->unsignedInteger('locale_id');
+            $table->string('locale_iso');
             $table->string('title');
             $table->text('preview_content')->nullable();
             $table->text('content')->nullable();
             $table->string('seo_title')->nullable();
             $table->string('seo_key_words')->nullable();
             $table->text('seo_descritpion')->nullable();
+            $table->unique(['article_id','locale_iso']);
+            $table->foreign('article_id')->references('id')->on('content_articles')->onDelete('cascade');
             $table->timestamps();
         });
     }
