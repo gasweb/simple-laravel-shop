8c2cc3233098980ddd855c445bdbb2d767279579
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-03-16 10:15:22 +0300

    Adds template

---
+++ resources/views/public/main/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.app')
+@section('content')
+    <h1 class="text-center">{{ trans('public/main/index.header') }}</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum dolore, dolorum et fugiat illo necessitatibus pariatur perspiciatis possimus provident quae quaerat ratione, rem sunt veniam! Aliquid cupiditate nostrum optio.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dignissimos illo non, officiis possimus quasi velit. Aliquam dolore enim facere illo modi nulla, possimus tempora velit voluptas voluptatum. Facilis, libero!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum dolore, dolorum et fugiat illo necessitatibus pariatur perspiciatis possimus provident quae quaerat ratione, rem sunt veniam! Aliquid cupiditate nostrum optio.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dignissimos illo non, officiis possimus quasi velit. Aliquam dolore enim facere illo modi nulla, possimus tempora velit voluptas voluptatum. Facilis, libero!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum dolore, dolorum et fugiat illo necessitatibus pariatur perspiciatis possimus provident quae quaerat ratione, rem sunt veniam! Aliquid cupiditate nostrum optio.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dignissimos illo non, officiis possimus quasi velit. Aliquam dolore enim facere illo modi nulla, possimus tempora velit voluptas voluptatum. Facilis, libero!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum dolore, dolorum et fugiat illo necessitatibus pariatur perspiciatis possimus provident quae quaerat ratione, rem sunt veniam! Aliquid cupiditate nostrum optio.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dignissimos illo non, officiis possimus quasi velit. Aliquam dolore enim facere illo modi nulla, possimus tempora velit voluptas voluptatum. Facilis, libero!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum dolore, dolorum et fugiat illo necessitatibus pariatur perspiciatis possimus provident quae quaerat ratione, rem sunt veniam! Aliquid cupiditate nostrum optio.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dignissimos illo non, officiis possimus quasi velit. Aliquam dolore enim facere illo modi nulla, possimus tempora velit voluptas voluptatum. Facilis, libero!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum dolore, dolorum et fugiat illo necessitatibus pariatur perspiciatis possimus provident quae quaerat ratione, rem sunt veniam! Aliquid cupiditate nostrum optio.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dignissimos illo non, officiis possimus quasi velit. Aliquam dolore enim facere illo modi nulla, possimus tempora velit voluptas voluptatum. Facilis, libero!
+    </p>
+@endsection
